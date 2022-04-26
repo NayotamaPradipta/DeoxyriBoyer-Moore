e3b7c548e7d8d68a9af4d927e7f305e800a9de35
Nayotama Pradipta <n4y0t4m4@gmail.com>
2022-04-27 02:42:13 +0700

    Add more regex parsing

--- src/algorithm/fileProcess.go
+++ src/algorithm/fileProcess.go
@@ -1,5 +1,5 @@
 package algorithm
-
+// File untuk pengecekan regex
 import (
 	"io/ioutil"
 	"path/filepath"
@@ -26,5 +26,45 @@ func IsValidString(dna string) bool {
 		return match
 	} else {
 		return false
+	}
+}
+
+func IsValidSearchDiseaseOnly(search string) bool {
+	// Regex Parsing 
+	match, err := regexp.MatchString(`([A-Z][a-z]+|[A-Z]+)`, search)
+	if err == nil {
+		if match {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		panic(err.Error())
+	}
+} 
+
+func IsValidSearchDateOnly(search string) bool {
+	match, err := regexp.MatchString(`^(3[01]|[12][0-9]|0?[1-9])\s(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)\s\d{4}$`, search)
+	if err == nil {
+		if match {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		panic(err.Error())
+	}
+}
+
+func IsValidSearchDateAndDisease(search string) bool {
+	match, err := regexp.MatchString(`((3[01]|[12][0-9]|0?[1-9])\s(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)\s\d{4}\s([A-Z][a-z]+|[A-Z]+))|(([A-Z][a-z]+|[A-Z]+)\s(3[01]|[12][0-9]|0?[1-9])\s(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)\s\d{4})`, search)
+	if err == nil {
+		if match {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		panic(err.Error())
 	}
 }
