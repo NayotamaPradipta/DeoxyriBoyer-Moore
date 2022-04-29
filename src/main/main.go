e4bb2ad4af05aa876d885af8ba16f2e0f5ca4852
Nayotama Pradipta <n4y0t4m4@gmail.com>
2022-04-29 20:24:43 +0700

    minor change

--- src/main/main.go
+++ src/main/main.go
@@ -1,69 +1,70 @@
 package main
 
 import (
-	"fmt"
 	"database/sql"
-	"net/http"
+	"fmt"
 	"log"
+	"net/http"
+	"strconv"
+	"time"
+
 	"github.com/NayotamaPradipta/DeoxyriBoyer-Moore/src/algorithm"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
-	"time"
-	"strconv"
 )
 
 func main() {
 	/*
-	// Asumsi sequence DNA pengguna > sequence penyakit
-	dnaToTest := algorithm.GetDNASequenceFromFile("dnaInput.txt")
-	disease := "GATC"
-	fmt.Println(dnaToTest)
-	if algorithm.IsValidString(dnaToTest) {
-		if !algorithm.StartBoyerMoore(dnaToTest, disease) {
-			fmt.Println("Disease not detected with Boyer-Moore!")
+		// Asumsi sequence DNA pengguna > sequence penyakit
+		dnaToTest := algorithm.GetDNASequenceFromFile("dnaInput.txt")
+		disease := "GATC"
+		fmt.Println(dnaToTest)
+		if algorithm.IsValidString(dnaToTest) {
+			if !algorithm.StartBoyerMoore(dnaToTest, disease) {
+				fmt.Println("Disease not detected with Boyer-Moore!")
+			} else {
+				fmt.Println("Disease detected with Boyer-Moore!")
+			}
+			if !algorithm.StartKMP(dnaToTest, disease) {
+				fmt.Println("Disease not detected with Boyer-Moore!")
+			} else {
+				fmt.Println("Disease detected with KMP!")
+			}
 		} else {
-			fmt.Println("Disease detected with Boyer-Moore!")
+			fmt.Println("Invalid DNA String!")
 		}
-		if !algorithm.StartKMP(dnaToTest, disease) {
-			fmt.Println("Disease not detected with Boyer-Moore!")
+		klinefelter := algorithm.GetDNASequenceFromFile("Klinefelter.txt")
+		if algorithm.IsValidString(klinefelter){
+			todb.InsertNewDisease("Klinefelter", klinefelter, "")
 		} else {
-			fmt.Println("Disease detected with KMP!")
+			fmt.Println("Invalid Disease DNA!")
 		}
-	} else {
-		fmt.Println("Invalid DNA String!")
-	}
-	klinefelter := algorithm.GetDNASequenceFromFile("Klinefelter.txt")
-	if algorithm.IsValidString(klinefelter){
-		todb.InsertNewDisease("Klinefelter", klinefelter, "")
-	} else {
-		fmt.Println("Invalid Disease DNA!")
-	}
-	fmt.Println(todb.SELECTDNA("x", ""))
-	// Testing searching 
-	if algorithm.IsValidSearchDiseaseOnly("testDisease") {
-		todb.SELECTDNA("testDisease", "")
-	} 
-	if algorithm.IsValidSearchDateAndDisease("HIV 20 September 1999") {
-		fmt.Println("Valid!")
-	}
-	if algorithm.IsValidSearchDateOnly("11 April 2020"){
-		fmt.Println("Valid!")
-	}
-	if algorithm.IsValidSearchDateAndDisease("19 October 2020 Klinefelter"){
-		fmt.Println("Valid!")
-	}
-	if !algorithm.IsValidSearchDateAndDisease("32 December 2020 HIV"){
-		fmt.Println("Invalid!")
-	}
-	todb.SELECTRIWAYAT("32 December 2020 HIV", "")
+		fmt.Println(todb.SELECTDNA("x", ""))
+		// Testing searching
+		if algorithm.IsValidSearchDiseaseOnly("testDisease") {
+			todb.SELECTDNA("testDisease", "")
+		}
+		if algorithm.IsValidSearchDateAndDisease("HIV 20 September 1999") {
+			fmt.Println("Valid!")
+		}
+		if algorithm.IsValidSearchDateOnly("11 April 2020"){
+			fmt.Println("Valid!")
+		}
+		if algorithm.IsValidSearchDateAndDisease("19 October 2020 Klinefelter"){
+			fmt.Println("Valid!")
+		}
+		if !algorithm.IsValidSearchDateAndDisease("32 December 2020 HIV"){
+			fmt.Println("Invalid!")
+		}
+		todb.SELECTRIWAYAT("32 December 2020 HIV", "")
 
-	todb.InsertNewPrediction("11 April 2020", "Kaori Miyazono", "Klinefelter", "False", "")
-	// Manual input buat testing backend
-	INSERT INTO hasilprediksi VALUES ("10 August 2020", "Ken Kaneki", "Ghoul", "True");
-	INSERT INTO hasilprediksi VALUES ("27 October 2021", "Chizuru Ichinose", "Kawaiism", "True");
-	INSERT INTO hasilprediksi VALUES ("27 October 2021", "Gabimaru", "Klinefelter", "False");
-	riwayat := todb.SELECTRIWAYAT("Klinefelter 11 April 2020", "")
-	fmt.Println(riwayat)
+		todb.InsertNewPrediction("11 April 2020", "Kaori Miyazono", "Klinefelter", "False", "")
+		// Manual input buat testing backend
+		INSERT INTO hasilprediksi VALUES ("10 August 2020", "Ken Kaneki", "Ghoul", "True");
+		INSERT INTO hasilprediksi VALUES ("27 October 2021", "Chizuru Ichinose", "Kawaiism", "True");
+		INSERT INTO hasilprediksi VALUES ("27 October 2021", "Gabimaru", "Klinefelter", "False");
+		riwayat := todb.SELECTRIWAYAT("Klinefelter 11 April 2020", "")
+		fmt.Println(riwayat)
 	*/
 	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/tesdna")
 	err = db.Ping()
@@ -71,151 +72,155 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.Close()
-	
-	router := gin.Default();
-	
+
+	router := gin.Default()
+
 	type jenispenyakit struct {
-		Id_penyakit               int       `json:"id_penyakit"`
-		Nama_penyakit             string    `json:"nama_penyakit"`
-		Rantai_dna                string    `json:"rantai_dna"`
+		Id_penyakit   int    `json:"id_penyakit"`
+		Nama_penyakit string `json:"nama_penyakit"`
+		Rantai_dna    string `json:"rantai_dna"`
 	}
-	
+
 	type hasilprediksi struct {
-		Tanggal_prediksi          string    `json:"tanggal_prediksi"`
-		Nama_pasien               string    `json:"nama_pasien"`
-		Penyakit_terprediksi      string    `json:"penyakit_terprediksi"`
-		Status_terprediksi        string    `json:"status_terprediksi"`
+		Tanggal_prediksi     string `json:"tanggal_prediksi"`
+		Nama_pasien          string `json:"nama_pasien"`
+		Penyakit_terprediksi string `json:"penyakit_terprediksi"`
+		Status_terprediksi   string `json:"status_terprediksi"`
 	}
 
 	router.GET("/disease/:penyakit_terprediksi", func(c *gin.Context) {
 		var (
-			hp hasilprediksi
-			hPs []hasilprediksi
-		    result gin.H 
+			hp     hasilprediksi
+			hPs    []hasilprediksi
+			result gin.H
 		)
 		penyakit_terprediksi := c.Param("penyakit_terprediksi")
-		if (algorithm.IsValidSearchDiseaseOnly(penyakit_terprediksi)){
+		if algorithm.IsValidSearchDiseaseOnly(penyakit_terprediksi) {
 			rows, err := db.Query("SELECT tanggal_prediksi, nama_pasien, penyakit_terprediksi, status_terprediksi FROM hasilprediksi WHERE penyakit_terprediksi = ?;", penyakit_terprediksi)
 			if err != nil {
 				result = gin.H{
-					"data" : "null",
-					"Error" : err.Error(),
+					"data":  "null",
+					"Error": err.Error(),
 				}
 			} else {
-				for rows.Next(){
+				for rows.Next() {
 					err = rows.Scan(&hp.Tanggal_prediksi, &hp.Nama_pasien, &hp.Penyakit_terprediksi, &hp.Status_terprediksi)
 					hPs = append(hPs, hp)
 					if err != nil {
 						result = gin.H{
-							"data" : "null",
-							"Error" : err.Error(),
+							"data":  "null",
+							"Error": err.Error(),
 						}
 					} else {
-						result = gin.H {
-							"data" : hPs,
+						result = gin.H{
+							"data": hPs,
 						}
 					}
+
 				}
 				defer rows.Close()
 			}
-			c.JSON(http.StatusOK, result)
+			c.IndentedJSON(http.StatusOK, result)
 		} else {
 			result = gin.H{
 				"data": "Invalid",
 			}
-			c.JSON(http.StatusOK, result)
-		} 
-
+			c.IndentedJSON(http.StatusOK, result)
+		}
 
 	})
 	router.GET("/date/:tanggal_prediksi", func(c *gin.Context) {
 		var (
-			hp hasilprediksi
-			hPs []hasilprediksi
-		    result gin.H 
+			hp     hasilprediksi
+			hPs    []hasilprediksi
+			result gin.H
 		)
 		tanggal_prediksi := c.Param("tanggal_prediksi")
-		if (algorithm.IsValidSearchDateOnly(tanggal_prediksi)){
+		tanggal_prediksi = tanggal_prediksi[:2] + " " + tanggal_prediksi[2:]
+		tanggal_prediksi = tanggal_prediksi[:len(tanggal_prediksi)-4] + " " + tanggal_prediksi[len(tanggal_prediksi)-4:]
+		if algorithm.IsValidSearchDateOnly(tanggal_prediksi) {
 			rows, err := db.Query("SELECT tanggal_prediksi, nama_pasien, penyakit_terprediksi, status_terprediksi FROM hasilprediksi WHERE tanggal_prediksi = ?;", tanggal_prediksi)
 			if err != nil {
 				result = gin.H{
-					"data" : "null",
-					"Error" : err.Error(),
+					"data":  "null",
+					"Error": err.Error(),
 				}
 			} else {
-				for rows.Next(){
+				for rows.Next() {
 					err = rows.Scan(&hp.Tanggal_prediksi, &hp.Nama_pasien, &hp.Penyakit_terprediksi, &hp.Status_terprediksi)
 					hPs = append(hPs, hp)
 					if err != nil {
 						result = gin.H{
-							"data" : "null",
-							"Error" : err.Error(),
+							"data":  "null",
+							"Error": err.Error(),
 						}
 					} else {
-						result = gin.H {
-							"data" : hPs,
+						result = gin.H{
+							"data": hPs,
 						}
 					}
 				}
 				defer rows.Close()
 			}
-			c.JSON(http.StatusOK, result)
+			c.IndentedJSON(http.StatusOK, result)
 		} else {
 			result = gin.H{
 				"data": "Invalid",
 			}
-			c.JSON(http.StatusOK, result)
-		} 
+			c.IndentedJSON(http.StatusOK, result)
+		}
 	})
 	router.GET("/dnd/:penyakit_terprediksi/:tanggal_prediksi", func(c *gin.Context) {
 		var (
-			hp hasilprediksi
-			hPs []hasilprediksi
-		    result gin.H 
+			hp     hasilprediksi
+			hPs    []hasilprediksi
+			result gin.H
 		)
 		penyakit_terprediksi := c.Param("penyakit_terprediksi")
 		tanggal_prediksi := c.Param("tanggal_prediksi")
+		tanggal_prediksi = tanggal_prediksi[:2] + " " + tanggal_prediksi[2:]
+		tanggal_prediksi = tanggal_prediksi[:len(tanggal_prediksi)-4] + " " + tanggal_prediksi[len(tanggal_prediksi)-4:]
 		combine := tanggal_prediksi + " " + penyakit_terprediksi
-		if (algorithm.IsValidSearchDateAndDisease(combine)){
+		if algorithm.IsValidSearchDateAndDisease(combine) {
 			rows, err := db.Query("SELECT tanggal_prediksi, nama_pasien, penyakit_terprediksi, status_terprediksi FROM hasilprediksi WHERE tanggal_prediksi = ? AND penyakit_terprediksi = ?;", tanggal_prediksi, penyakit_terprediksi)
 			if err != nil {
 				result = gin.H{
-					"data" : "null",
-					"Error" : err.Error(),
+					"data":  "null",
+					"Error": err.Error(),
 				}
 			} else {
-				for rows.Next(){
+				for rows.Next() {
 					err = rows.Scan(&hp.Tanggal_prediksi, &hp.Nama_pasien, &hp.Penyakit_terprediksi, &hp.Status_terprediksi)
 					hPs = append(hPs, hp)
 					if err != nil {
 						result = gin.H{
-							"data" : "null",
-							"Error" : err.Error(),
+							"data":  "null",
+							"Error": err.Error(),
 						}
 					} else {
-						result = gin.H {
-							"data" : hPs,
+						result = gin.H{
+							"data": hPs,
 						}
 					}
 				}
 				defer rows.Close()
 			}
-			c.JSON(http.StatusOK, result)
+			c.IndentedJSON(http.StatusOK, result)
 		} else {
 			result = gin.H{
 				"data": "Invalid",
 				"test": tanggal_prediksi,
 			}
-			c.JSON(http.StatusOK, result)
-		} 
+			c.IndentedJSON(http.StatusOK, result)
+		}
 
 	})
 
-	router.POST("/insertDisease", func(c *gin.Context){
+	router.POST("/insertDisease", func(c *gin.Context) {
 		id_penyakit, _ := strconv.Atoi(c.PostForm("id_penyakit"))
 		nama_penyakit := c.PostForm("nama_penyakit")
 		rantai_dna := c.PostForm("rantai_dna")
-		if (algorithm.IsValidString(rantai_dna)){
+		if algorithm.IsValidString(rantai_dna) {
 			query := fmt.Sprintf(`INSERT INTO tesdna.jenispenyakit (id_penyakit, nama_penyakit, rantai_dna) SELECT * FROM (SELECT '%d', '%s', '%s') AS tmp WHERE NOT EXISTS (SELECT nama_penyakit FROM tesdna.jenispenyakit WHERE nama_penyakit='%s') LIMIT 1`, id_penyakit, nama_penyakit, rantai_dna, nama_penyakit)
 			insert, err := db.Prepare(query)
 
@@ -224,25 +229,25 @@ func main() {
 			}
 			_, err = insert.Exec(id_penyakit, nama_penyakit, rantai_dna)
 			if err != nil {
-				c.JSON(http.StatusOK, gin.H {
-					"Message" : "Insertion of new disease is unsuccessful",
+				c.IndentedJSON(http.StatusOK, gin.H{
+					"Message": "Insertion of new disease is unsuccessful",
 				})
 			}
-			c.JSON(http.StatusOK, gin.H {
-				"Message" : "Insertion of new disease is successful",
+			c.IndentedJSON(http.StatusOK, gin.H{
+				"Message": "Insertion of new disease is successful",
 			})
 		} else {
-			c.JSON(http.StatusOK, gin.H{"Message": "Invalid Rantai DNA"})
+			c.IndentedJSON(http.StatusOK, gin.H{"Message": "Invalid Rantai DNA"})
 		}
 	})
-	router.POST("/insertDNA", func(c *gin.Context){
+	router.POST("/insertDNA", func(c *gin.Context) {
 		nama_penyakit := c.PostForm("nama_penyakit")
 		query := fmt.Sprintf(`SELECT rantai_dna FROM jenispenyakit WHERE nama_penyakit = "%s"`, nama_penyakit)
 		var rdna string
 		err = db.QueryRow(query).Scan(&rdna)
 		if err != nil {
-			c.JSON(http.StatusOK, gin.H {
-				"Message" : err.Error(),
+			c.IndentedJSON(http.StatusOK, gin.H{
+				"Message": err.Error(),
 			})
 		} else {
 			nama_pasien := c.PostForm("nama_pasien")
@@ -260,8 +265,8 @@ func main() {
 				query := fmt.Sprintf(`INSERT IGNORE INTO hasilprediksi VALUES ('%s', '%s', '%s', '%s')`, date, nama_pasien, nama_penyakit, diagnosis)
 				insert, err := db.Query(query)
 				if err != nil {
-					c.JSON(http.StatusOK, gin.H {
-						"Message" : "Invalid Input file!",
+					c.IndentedJSON(http.StatusOK, gin.H{
+						"Message": "Invalid Input file!",
 					})
 				} else {
 					var hP hasilprediksi
@@ -269,14 +274,15 @@ func main() {
 					hP.Nama_pasien = nama_pasien
 					hP.Penyakit_terprediksi = nama_penyakit
 					hP.Status_terprediksi = diagnosis
-					c.JSON(http.StatusOK, gin.H {
-						"HasilPrediksi" : hP,
+					c.IndentedJSON(http.StatusOK, gin.H{
+						"HasilPrediksi": hP,
 					})
+
 				}
 				defer insert.Close()
 			} else {
-				c.JSON(http.StatusOK, gin.H {
-					"Message" : "Invalid Input file!",
+				c.IndentedJSON(http.StatusOK, gin.H{
+					"Message": "Invalid Input file!",
 				})
 			}
 		}
